6699972a795edfd98d4978eec8e470a7fcba32c7
Anand A. Joshi <ajoshi@usc.edu>
2023-02-08 16:36:12 -0800

    update

--- dev/sample_fmri_proc_openneuro_fsl.py
+++ dev/sample_fmri_proc_openneuro_fsl.py
@@ -26,6 +26,11 @@ fmri = fmri_orig[:-4] + '.nii.gz'
 zip_cmd = f'gzip -cvf {fmri_orig} > {fmri}'
 os.system(zip_cmd)
 
+fmri resample to regular grid
+
+
+
+
 # create fMRI mean image to be used as registration target
 fmri_example = fmri_orig[:-4] + '.mean.nii.gz'
 
@@ -36,39 +41,28 @@ os.system(mean_cmd)
 
 # Register t1 to fMRI
 
-linreg_bin = '/home/ajoshi/projects/rodreg/dev/aligner.py'
+linreg_bin = 'flirt'
 t1_bse_lin = t1[:-7] + '.lin.nii.gz'
 
-lin_align_cmd = f'{linreg_bin} {t1_bse} {fmri_example} {t1_bse_lin} -l cc'
-
+lin_align_cmd = f'{linreg_bin} -in {t1_bse} -ref {fmri_example} -out {t1_bse_lin} -dof 6'
 os.system(lin_align_cmd)
 
 
 
-# warp atlas to subj
+# warp subject to atlas
 
-atlas = '/deneb_disk/RodentTools/Atlases/DSURQE_40micron_UCLA/DSURQE_40micron_average_masked.nii.gz'
-linreg_bin = '/home/ajoshi/projects/rodreg/dev/aligner.py'
+atlas = '/deneb_disk/RodentTools/Atlases/DSURQE_40micron_UCLA/DSURQE_40micron_64_average_masked.nii.gz'
+linreg_bin = 'flirt'
 t1_bse_lin = t1[:-7] + '.lin.atlas.nii.gz'
-lin_align_cmd = f'{linreg_bin} {t1_bse} {atlas} {t1_bse_lin} -l cc'
+mat_file = t1[:-7] + '.lin.mat'
+
+lin_align_cmd = f'{linreg_bin} -in {t1_bse} -ref {atlas} -out {t1_bse_lin} -omat {mat_file}'
 os.system(lin_align_cmd)
 
 
 
-# Compose deformation from linear and nonlinear
-apply_warp_to_4D(ddf_nonlinear_file,ddf_linear_file, ddf_combined_file)
 
-
-
-# Map fMRI to atlas space
-
-
-# Map fMRI to atlas space
-apply_warp_to_4D(ddf_combined_file, fmri, output_fmri)
-
-
-
-
-
-
-
+# Warp fmri to atlas
+fmri_warped = fmri[:-7] + '.atlas.nii.gz'
+linwarp_cmd = f'{linreg_bin} -in {fmri} -ref {atlas} -out {fmri_warped} -applyxfm -init {mat_file} -interp trilinear'
+os.system(linwarp_cmd)
