fe0f52184c07980b8e0cfdbb3d25871ec9ddf768
Anand A. Joshi <ajoshi@usc.edu>
2023-02-10 10:19:23 -0800

    update

---
+++ dev/main_brainsync.py
@@ -0,0 +1,61 @@
+#||AUM||
+#||Shree Ganeshaya Namaha||
+
+import nibabel as nb
+
+from brainsync import brainSync, normalizeData
+import numpy as np
+
+import scipy as sp
+
+
+atlas = '/deneb_disk/RodentTools/Atlases/DSURQE_40micron_UCLA/DSURQE_40micron_64_average_masked.nii.gz'
+
+
+v = nb.load(atlas)
+
+msk = v.get_fdata() > 0
+
+
+print(v)
+
+
+
+sub1 = '/home/ajoshi/projects/rodfmri/dev/test_cases/sub-jgrADc1NT/ses-1/func/sub-jgrADc1NT_ses-1_task-rest_acq-EPI_b.atlas.nii.gz'
+
+sub2 = '/home/ajoshi/projects/rodfmri/dev/test_cases/sub-jgrADc1NT/ses-2/func/sub-jgrADc1NT_ses-2_task-rest_acq-EPI_b.atlas.nii.gz'
+
+
+f1 = nb.load(sub1)
+f2 = nb.load(sub2)
+
+f1img = f1.get_fdata()[msk].T
+f2img = f2.get_fdata()[msk].T
+
+print(f1img.shape)
+print(f2img.shape)
+
+f1img, _, _ =  normalizeData(f1img)
+f2img, _, _ =  normalizeData(f2img)
+
+
+diff = np.sqrt(np.sum((f1img - f2img)**2, axis=0))
+
+
+f2img_sync, _ = brainSync(f1img, f2img)
+
+diff_sync = np.sqrt(np.sum((f1img - f2img_sync)**2, axis=0))
+
+
+vout = np.zeros(v.shape)
+vout[msk] = diff
+
+unsyn_img = nb.Nifti1Image(vout, f1.affine, f1.header)
+unsyn_img.to_filename('unsynced.nii.gz')
+
+vout = np.zeros(v.shape)
+vout[msk] = diff_sync
+
+syn_img = nb.Nifti1Image(vout, f1.affine, f1.header)
+syn_img.to_filename('synced.nii.gz')
+
--- dev/sample_fmri_proc_openneuro_fsl.py
+++ dev/sample_fmri_proc_openneuro_fsl.py
@@ -1,10 +1,15 @@
 import os
 
+subid = 'sub-jgrADc1NT'
+sess = 2
 
-t1_orig = '/home/ajoshi/projects/rodfmri/dev/test_cases/sub-jgrADc1NT/ses-1/anat/sub-jgrADc1NT_ses-1_acq-RARE_T2w.nii'
+t1_orig = f'/home/ajoshi/projects/rodfmri/dev/test_cases/{subid}/ses-{sess}/anat/{subid}_ses-{sess}_acq-RARE_T2w.nii'
+fmri_orig = f'/home/ajoshi/projects/rodfmri/dev/test_cases/{subid}/ses-{sess}/func/{subid}_ses-{sess}_task-rest_acq-EPI_bold.nii'
+
 t1 = t1_orig[:-4] + '_ds.nii.gz'
 t1_bse = t1[:-4] + '.bse.nii.gz'
 resample_bin = '/home/ajoshi/BrainSuite21a/svreg/bin/svreg_resample.sh'
+atlas = '/deneb_disk/RodentTools/Atlases/DSURQE_40micron_UCLA/DSURQE_40micron_64_average_masked.nii.gz'
 
 t1_resample_cmd = f'{resample_bin} {t1_orig} {t1} -size 100 100 100'
 os.system(t1_resample_cmd)
@@ -19,14 +24,13 @@ os.system(t1_bse_cmd)
 # zip fmri
 
 
-fmri_orig = '/home/ajoshi/projects/rodfmri/dev/test_cases/sub-jgrADc1NT/ses-1/func/sub-jgrADc1NT_ses-1_task-rest_acq-EPI_bold.nii'
 
-fmri = fmri_orig[:-4] + '.nii.gz'
+fmri = fmri_orig #[:-4] + '.nii.gz'
 
-zip_cmd = f'gzip -cvf {fmri_orig} > {fmri}'
-os.system(zip_cmd)
+#zip_cmd = f'gzip -cvf {fmri_orig} > {fmri}'
+#os.system(zip_cmd)
 
-fmri resample to regular grid
+#fmri resample to regular grid
 
 
 
@@ -42,21 +46,20 @@ os.system(mean_cmd)
 # Register t1 to fMRI
 
 linreg_bin = 'flirt'
-t1_bse_lin = t1[:-7] + '.lin.nii.gz'
+t1_bse_fmri = t1[:-7] + '.fmri.nii.gz'
 
-lin_align_cmd = f'{linreg_bin} -in {t1_bse} -ref {fmri_example} -out {t1_bse_lin} -dof 6'
+lin_align_cmd = f'{linreg_bin} -in {t1_bse} -ref {fmri_example} -out {t1_bse_fmri} -dof 6'
 os.system(lin_align_cmd)
 
 
 
 # warp subject to atlas
 
-atlas = '/deneb_disk/RodentTools/Atlases/DSURQE_40micron_UCLA/DSURQE_40micron_64_average_masked.nii.gz'
 linreg_bin = 'flirt'
 t1_bse_lin = t1[:-7] + '.lin.atlas.nii.gz'
 mat_file = t1[:-7] + '.lin.mat'
 
-lin_align_cmd = f'{linreg_bin} -in {t1_bse} -ref {atlas} -out {t1_bse_lin} -omat {mat_file}'
+lin_align_cmd = f'{linreg_bin} -in {t1_bse_fmri} -ref {atlas} -out {t1_bse_lin} -omat {mat_file}'
 os.system(lin_align_cmd)
 
 
