39808658c8baaaf547b195ff5f9bdae191035248
Anand A. Joshi <ajoshi@usc.edu>
2025-01-04 12:50:21 -0800

    update

--- dev/main_openneuro_preproc.py
+++ dev/main_openneuro_preproc.py
@@ -1,6 +1,8 @@
 import glob
 from fmri_proc_fsl import fmri_proc_fsl
 import os
+from myutils import plot_fmri_overlay
+from itertools import product
 
 
 sublist = glob.glob('/deneb_disk/RodentTools/data/openneuro/ds001890/sub*')
@@ -10,22 +12,25 @@ atlas = '/deneb_disk/RodentTools/Atlases/DSURQE_40micron_UCLA/DSURQE_40micron_64
 # set environment variable BrainSuiteMCRPath to the path of BrainSuite Matlab Compiler Runtime
 os.environ['BrainSuiteMCR'] = '/home/ajoshi/Software/MATLAB/MATLAB_Runtime/R2023a'
 
-for s in sublist:
+# loop over subjects and sessions. Use single for loop using zip
+# to loop over both subjects and sessions simultaneously
+# and avoid code duplication
+sessions = [1, 2]
+
+for s, sess in product(sublist, sessions):
+
+    print(f'Processing subject {s} session {sess}...')
 
     dirname, subid = os.path.split(s)
-    sess = 1
     t1_orig = f'{dirname}/{subid}/ses-{sess}/anat/{subid}_ses-{sess}_acq-RARE_T2w.nii'
     fmri_orig = f'{dirname}/{subid}/ses-{sess}/func/{subid}_ses-{sess}_task-rest_acq-EPI_bold.nii'
-    fmri2atlas = fmri_orig[:-7] + '.filt.atlas.nii.gz'
+    fmri2atlas = fmri_orig[:-4] + '.filt.atlas.nii.gz'
 
     if os.path.isfile(fmri_orig) and not os.path.isfile(fmri2atlas):
         fmri_proc_fsl(t1_orig, fmri_orig, BrainSuitePath, atlas)
 
+    if os.path.isfile(fmri_orig) and os.path.isfile(fmri2atlas):
+        plot_fmri_overlay(fmri2atlas, atlas, threshold=3, time_point=3, average_time=False)
 
-    sess = 2
-    t1_orig = f'{dirname}/{subid}/ses-{sess}/anat/{subid}_ses-{sess}_acq-RARE_T2w.nii'
-    fmri_orig = f'{dirname}/{subid}/ses-{sess}/func/{subid}_ses-{sess}_task-rest_acq-EPI_bold.nii'
-    fmri2atlas = fmri_orig[:-7] + '.filt.atlas.nii.gz'
 
-    if os.path.isfile(fmri_orig) and not os.path.isfile(fmri2atlas):
-        fmri_proc_fsl(t1_orig, fmri_orig, BrainSuitePath, atlas)
+
---
+++ dev/myutils.py
@@ -0,0 +1,40 @@
+"""
+This module provides utilities for working with MRI and fMRI data.
+"""
+
+from nilearn import image, plotting
+
+from matplotlib import pyplot as plt
+
+def plot_fmri_overlay(fmri_image_path, anatomical_image_path, 
+                       threshold=3, time_point=3, average_time=False):
+    """
+    Plot an fMRI activation map overlaid on an anatomical image.
+    
+    Parameters:
+    - fmri_image_path (str): Path to the 4D fMRI data (NIfTI file).
+    - anatomical_image_path (str): Path to the anatomical image (NIfTI file).
+    - threshold (float): Threshold for activation values in the fMRI map.
+    - time_point (int, optional): Specific time point to extract from the 4D fMRI image.
+    - average_time (bool): Whether to average across time points if time_point is not specified.
+    """
+    # Load images
+    fmri_img_4d = image.load_img(fmri_image_path)
+    anat_img = image.load_img(anatomical_image_path)
+    
+    # Handle 4D fMRI data
+    if fmri_img_4d.ndim == 4:
+        if time_point is not None:
+            fmri_img_3d = image.index_img(fmri_img_4d, time_point)
+        elif average_time:
+            fmri_img_3d = image.mean_img(fmri_img_4d)
+        else:
+            raise ValueError("Specify either 'time_point' or enable 'average_time'.")
+    else:
+        fmri_img_3d = fmri_img_4d  # Already 3D
+    
+    # Plot overlay
+    plotting.plot_stat_map(fmri_img_3d, bg_img=anat_img, threshold=threshold, alpha=0.5,
+                           display_mode='ortho', dim=-0.5, title='fMRI Activation Overlay', output_file=fmri_image_path[:-7] + '.png')
+    #plotting.show()
+    #plt.pause(1)  # Pause for a short time to allow the plot to render
